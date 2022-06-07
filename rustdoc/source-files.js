d6ac28797701402c2420c3d05aeb33d47478d5c8
hanabi1224 <hanabi1224@users.noreply.github.com>
2022-06-07 15:37:49 +0000

    deploy: 3bae9501e10c9c01eb965e7695ec715ad6672630

--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -1,15 +1,15 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["cairo"] = {"name":"","files":["flags.rs","helper.rs","lib.rs","memory.rs","runner.rs","word.rs"]};
 sourcesIndex["commitment_dlog"] = {"name":"","files":["chunked.rs","combine.rs","commitment.rs","evaluation_proof.rs","lib.rs","srs.rs"]};
 sourcesIndex["export_test_vectors"] = {"name":"","files":["main.rs","vectors.rs"]};
 sourcesIndex["flamegraph"] = {"name":"","files":["flamegraph.rs"]};
 sourcesIndex["groupmap"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["kimchi"] = {"name":"","dirs":[{"name":"circuits","dirs":[{"name":"lookup","dirs":[{"name":"tables","files":["mod.rs","xor.rs"]}],"files":["constraints.rs","lookups.rs","mod.rs"]},{"name":"polynomials","files":["chacha.rs","complete_add.rs","endomul_scalar.rs","endosclmul.rs","generic.rs","mod.rs","permutation.rs","poseidon.rs","turshi.rs","varbasemul.rs"]}],"files":["argument.rs","constraints.rs","domain_constant_evaluation.rs","domains.rs","expr.rs","gate.rs","macros.rs","mod.rs","polynomial.rs","scalars.rs","serialization_helper.rs","wires.rs"]}],"files":["alphas.rs","bench.rs","error.rs","lib.rs","linearization.rs","plonk_sponge.rs","proof.rs","prover.rs","prover_index.rs","verifier.rs","verifier_index.rs"]};
+sourcesIndex["kimchi"] = {"name":"","dirs":[{"name":"circuits","dirs":[{"name":"lookup","dirs":[{"name":"tables","files":["mod.rs","range_check.rs","xor.rs"]}],"files":["constraints.rs","index.rs","lookups.rs","mod.rs","runtime_tables.rs"]},{"name":"polynomials","dirs":[{"name":"range_check","files":["circuitgates.rs","gate.rs","mod.rs","witness.rs"]}],"files":["chacha.rs","complete_add.rs","endomul_scalar.rs","endosclmul.rs","generic.rs","mod.rs","permutation.rs","poseidon.rs","turshi.rs","varbasemul.rs"]}],"files":["argument.rs","constraints.rs","domain_constant_evaluation.rs","domains.rs","expr.rs","gate.rs","macros.rs","mod.rs","polynomial.rs","scalars.rs","serialization_helper.rs","wires.rs"]}],"files":["alphas.rs","bench.rs","error.rs","lib.rs","linearization.rs","plonk_sponge.rs","proof.rs","prover.rs","prover_index.rs","verifier.rs","verifier_index.rs"]};
 sourcesIndex["kimchi_visu"] = {"name":"","files":["lib.rs","witness.rs"]};
 sourcesIndex["mina_curves"] = {"name":"","dirs":[{"name":"pasta","dirs":[{"name":"curves","files":["mod.rs","pallas.rs","vesta.rs"]},{"name":"fields","files":["fp.rs","fq.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["lib.rs"]};
 sourcesIndex["mina_hasher"] = {"name":"","files":["lib.rs","poseidon.rs","roinput.rs"]};
 sourcesIndex["mina_signer"] = {"name":"","files":["keypair.rs","lib.rs","pubkey.rs","schnorr.rs","seckey.rs","signature.rs"]};
-sourcesIndex["o1_utils"] = {"name":"","files":["chunked_polynomial.rs","dense_polynomial.rs","evaluations.rs","field_helpers.rs","hasher.rs","lib.rs","math.rs","serialization.rs","types.rs"]};
+sourcesIndex["o1_utils"] = {"name":"","files":["adjacent_pairs.rs","chunked_polynomial.rs","dense_polynomial.rs","evaluations.rs","field_helpers.rs","hasher.rs","lib.rs","math.rs","serialization.rs","types.rs"]};
 sourcesIndex["ocaml_derive"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["ocaml_gen"] = {"name":"","files":["conv.rs","lib.rs"]};
 sourcesIndex["oracle"] = {"name":"","dirs":[{"name":"pasta","files":["fp_kimchi.rs","fp_legacy.rs","fq_kimchi.rs","fq_legacy.rs","mod.rs"]}],"files":["constants.rs","lib.rs","permutation.rs","poseidon.rs","sponge.rs"]};
